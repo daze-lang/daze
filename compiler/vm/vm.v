43910d73ec7473b13e2bbcca27211870f7315c0e
Mark Molnar <molnarpw@gmail.com>
2021-08-05 21:50:09 +0200

    sub, mul, div instructions added

--- compiler/vm/vm.v
+++ compiler/vm/vm.v
@@ -4,7 +4,10 @@ type Operand = string | int | f64
 
 enum Kind {
     push
-    plus
+    add
+    sub
+    div
+    mul
 }
 
 struct Instruction {
@@ -18,22 +21,62 @@ mut:
     ip int
 }
 
+fn (mut vm VM) push(op Operand) {
+    vm.stack << op
+    vm.ip++
+}
+
 fn (mut vm VM) exec(inst Instruction) {
     match inst.kind {
         .push {
-            vm.stack << inst.op
-            vm.ip++
+            vm.push(inst.op)
         }
-        .plus {
+        .add {
             right := vm.pop()
             left := vm.pop()
 
             if mut left is int && mut right is int {
-                vm.stack << left + right
+                vm.push(left + right)
             } else if left is f64 && mut right is f64 {
-                vm.stack << left + right
+                vm.push(left + right)
             } else if left is string && mut right is string {
-                vm.stack << left + right
+                vm.push(left + right)
+            }
+        }
+        .sub {
+            right := vm.pop()
+            left := vm.pop()
+
+            if mut left is int && mut right is int {
+                vm.push(left - right)
+            } else if left is f64 && mut right is f64 {
+                vm.push(left - right)
+            } else if left is string && mut right is string {
+                vm.push(0)
+            }
+        }
+        .mul {
+            right := vm.pop()
+            left := vm.pop()
+
+            if mut left is int && mut right is int {
+                vm.push(left * right)
+            } else if left is f64 && mut right is f64 {
+                vm.push(left * right)
+            } else if left is string && mut right is string {
+                vm.push(0)
+            }
+        }
+        .div {
+            right := vm.pop()
+            left := vm.pop()
+
+            if mut left is int && mut right is int {
+                vm.push(left / right)
+            } else if left is f64 && mut right is f64 {
+                vm.push(left / right)
+            } else if left is string && mut right is string {
+                vm.push(0)
             }
         }
     }
@@ -60,7 +103,15 @@ fn main() {
         op: 4
     })
     vm.exec(Instruction{
-        kind: .plus,
+        kind: .add,
+        op: ""
+    })
+    vm.exec(Instruction{
+        kind: .push,
+        op: 2
+    })
+    vm.exec(Instruction{
+        kind: .mul,
         op: ""
     })
 
