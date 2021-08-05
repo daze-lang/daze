11e9e98d54931431306c94b29297bac77c81190d
Mark Molnar <molnarpw@gmail.com>
2021-08-05 21:15:43 +0200

    Fix formatting

--- compiler/lexer/token.v
+++ compiler/lexer/token.v
@@ -13,9 +13,9 @@ pub enum TokenType {
     colon_equal        // :=
     equal              // =
     double_slash       //
-    string
-    identifier
-    number
+    string             // cstrings
+    identifier         // identifiers
+    number             // any number
     plus               // +
     plus_plus          // ++
     minus              // -
@@ -36,58 +36,57 @@ pub enum TokenType {
     close_square       // ]
     arrow_left         // <-
 
-    // keywords
-    kw_struct
-    kw_fn
-    kw_is
-    kw_return
-    kw_use
-    kw_raw
-    kw_implement
-    kw_if
-    kw_else
-    kw_elif
-    kw_for
-    kw_break
-    kw_in
-    kw_make
+    kw_struct          // `struct`
+    kw_fn              // `fn`
+    kw_is              // `is`
+    kw_return          // `ret`
+    kw_use             // `use`
+    kw_raw             // `raw`
+    kw_implement       // `implement`
+    kw_if              // `if`
+    kw_else            // `else`
+    kw_elif            // `elif`
+    kw_for             // `for`
+    kw_break           // `break`
+    kw_in              // `in`
+    kw_make            // `make`
 
-    raw_crystal_code
+    raw_crystal_code   // raw crystal code block
 
-    eof
+    eof                // indicating that there are no more tokens left
 }
 
 pub const tokens_map = map{
-  "open_paren": "(",
-  "close_paren":")",
-  "open_curly": "{",
-  "close_curly":"}",
-  "at":"@",
-  "semicolon":  ";",
-  "comma":      ",",
-  "colon":      ":",
-  "double_colon":        ":",
-  "colon_equal":"=",
-  "equal":      "=",
-  "double_slash":        "//",
-  "plus":       "+",
-  "plus_plus":  "++",
-  "minus":      "-",
-  "minus_minus":"--",
-  "div":        "/",
-  "mod":        "%",
-  "and_and":    "&&",
-  "not_equal":  "!=",
-  "equal_equal":"==",
-  "greater_than":        ">",
-  "greater_than_equal":  ">=",
-  "less_than_equal":     "<=",
-  "less_than":  "<",
-  "not":        "!",
-  "backtick":   "`",
-  "open_square":"[",
-  "close_square":        "]",
-  "arrow_left": "<-",
+  "open_paren"         :" (",
+  "close_paren"        :" )",
+  "open_curly"         :" {",
+  "close_curly"        :" }",
+  "at"                 :" @",
+  "semicolon"          :" ;",
+  "comma"              :" ,",
+  "colon"              :":" ,
+  "double_colon"       :"::" ,
+  "colon_equal"        :" =",
+  "equal"              :" =",
+  "double_slash"       :" //",
+  "plus"               :" +",
+  "plus_plus"          :" ++",
+  "minus"              :" -",
+  "minus_minus"        :" --",
+  "div"                :" /",
+  "mod"                :" %",
+  "and_and"            :" &&",
+  "not_equal"          :" !=",
+  "equal_equal"        :" ==",
+  "greater_than"       :" >",
+  "greater_than_equal" :" >=",
+  "less_than_equal"    :" <=",
+  "less_than"          :" <",
+  "not"                :" !",
+  "backtick"           :" `",
+  "open_square"        :" [",
+  "close_square"       :" ]",
+  "arrow_left"         :" <-",
 }
 
 pub struct Token {
